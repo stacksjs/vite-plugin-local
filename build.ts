2f7fb86e5dfecfb82705c7dadbbc6c8d1d15b691
Chris <chrisbreuer93@gmail.com>
2024-12-09 13:43:58 +0100

    chore: adjust build

--- build.ts
+++ build.ts
@@ -1,7 +1,31 @@
+import process from 'node:process'
 import { dts } from 'bun-plugin-dtsx'
 
-await Bun.build({
-  entrypoints: ['src/index.ts'],
+console.log('Building...')
+
+// await $`rm -rf ./dist`
+
+const result = await Bun.build({
+  entrypoints: ['./src/index.ts'],
   outdir: './dist',
-  plugins: [dts()],
+  format: 'esm',
+  target: 'node',
+  minify: true,
+  // splitting: true,
+  plugins: [
+    dts(),
+  ],
 })
+
+if (!result.success) {
+  console.error('Build failed')
+
+  for (const message of result.logs) {
+    // Bun will pretty print the message object
+    console.error(message)
+  }
+
+  process.exit(1)
+}
+
+console.log('Build complete')
